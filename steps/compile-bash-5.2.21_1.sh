aeacb8846706494e315cfced48a7b7f31b7df6d1
wangshuai <wang.lin.shuai@qq.com>
2024-03-17 21:42:22 +0800

    compile bash first

---
+++ steps/compile-bash-5.2.21_1.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+cd $LFS/sources
+
+rm -rf bash-5.2.21
+tar -xvf bash-5.2.21.tar.gz
+cd bash-5.2.21
+
+./configure --prefix=/usr \
+	--build=$(sh support/config.guess) \
+	--host=$LFS_TGT \
+	--without-bash-malloc
+make
+make DESTDIR=$LFS install
+ln -sv bash $LFS/bin/sh
+rm -rf $LFS/sources/bash-5.2.21
+
