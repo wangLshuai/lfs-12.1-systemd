e8a860f385ac019a9fbd6a3309ca9f65cb1fb7ef
wangshuai <wang.lin.shuai@qq.com>
2024-03-20 21:24:46 +0800

    clean lfs root

--- chroot-step.sh
+++ chroot-step.sh
@@ -91,3 +91,7 @@ chmod -v 600  /var/log/btmp
 /chroot-steps/compile-python-3.12.2_1.sh
 /chroot-steps/compile-textinfo-7.1_1.sh
 /chroot-steps/compile-util-linux-2.39.3_1.sh
+
+rm -rf /usr/share/{info,man,doc}/*
+find /usr/{lib,libexec} -name \*.la -delete
+rm -rf /tools
