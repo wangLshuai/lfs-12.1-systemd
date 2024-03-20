dc8d8eb77847659c801420897c8f73713dd6d4d8
wangshuai <wang.lin.shuai@qq.com>
2024-03-20 21:22:47 +0800

    add compile util-linux-2.39.3 first

--- chroot-step.sh
+++ chroot-step.sh
@@ -90,3 +90,4 @@ chmod -v 600  /var/log/btmp
 /chroot-steps/compile-perl-5.38.2_1.sh
 /chroot-steps/compile-python-3.12.2_1.sh
 /chroot-steps/compile-textinfo-7.1_1.sh
+/chroot-steps/compile-util-linux-2.39.3_1.sh
