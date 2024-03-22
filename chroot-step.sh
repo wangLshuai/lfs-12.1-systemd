d020ba0b5831c883b8bfa719a9454c3a0f848786
wangshuai <wang.lin.shuai@qq.com>
2024-03-22 19:30:31 +0800

    add compile iproute2-6.7.0

--- chroot-step.sh
+++ chroot-step.sh
@@ -157,3 +157,4 @@ rm -rf /tools
 /chroot-steps/compile-groff-1.23.0.sh
 /chroot-steps/compile-grub-2.12.sh
 /chroot-steps/compile-gzip-1.13.sh
+/chroot-steps/compile-iproute2-6.7.0.sh
---
+++ chroot-steps/compile-iproute2-6.7.0.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+cd /sources
+
+rm -rf iproute2-6.7.0
+tar -xvf iproute2-6.7.0.tar.xz
+cd iproute2-6.7.0
+
+sed -i /ARPD/d Makefile
+rm -fv man/man8/arpd.8
+
+make NETNS_RUN_DIR=/run/netns
+make SBINDIR=/usr/sbin install
+mkdir -v /usr/share/doc/iproute2-6.7.0
+cp -v COPYING README* /usr/share/doc/iproute2-6.7.0
+rm -rf /sources/iproute2-6.7.0
+
