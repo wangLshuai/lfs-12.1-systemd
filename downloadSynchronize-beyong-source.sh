6d5d0abec2856517aa8d50f8b26b506abf39a7f0
wangshuai <wang.lin.shuai@qq.com>
2024-06-05 11:02:55 +0800

    synchronize beyong sources to virtual disk

--- downloadSynchronize-beyong-source.sh
+++ downloadSynchronize-beyong-source.sh
@@ -1,6 +1,11 @@
 #!/bin/env bash
-# 
-
+#!/usr/bin/env bash
+#run in ubuntu-docker
+set -e
+if [ ! "$HOSTNAME" = "lfs-docker" ];then
+    echo "run this script in lfs-docker"
+    exit 1
+fi
 wget https://ftp.gnu.org/gnu/wget/wget-1.21.4.tar.gz --continue --directory-prefix=./beyong-sources
 wget https://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-9.6p1.tar.gz --continue --directory-prefix=./beyong-sources
 wget  https://github.com/libusb/libusb/releases/download/v1.0.27/libusb-1.0.27.tar.bz2 --continue --directory-prefix=./beyong-sources
@@ -13,3 +18,11 @@ echo "8ff21441faf2e8128e4810b3d6e49059  usbutils-017.tar.xz"  | md5sum -c
 echo "1fb61afe370e94f902a67e03eb39c51f  libusb-1.0.27.tar.bz2" |md5sum -c
 popd
 
+steps/remove-disk.sh
+steps/disk.sh
+steps/part-disk.sh
+steps/mount.sh
+cp -rf beyong-sources "$LFS"
+cp beyong-lfs-step.sh "$LFS"
+cp -rf beyong-lfs-steps "$LFS"
+steps/remove-disk.sh
